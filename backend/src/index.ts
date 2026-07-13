443849846c4aa3c410d4919741a80a60a134c10c
tomaszceremuga <tomaszceremuga>
2026-07-13 10:09:22 +0200

    api test

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,23 @@
 import express from "express"
 import cors from "cors"
+import { type Request, type Response } from 'express';
 
 const app = express()
 
 app.use(cors())
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("api is working")
 })
+
+app.post('/post1', (req: Request, res: Response) => {
+    const { text } = req.body;
+    console.log(text);
+    // res.json()
+
+    res.send('Got a POST request');
+});
 
 app.listen(4000, () => {
     console.log("Server running on port 4000")
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,21 +1,39 @@
 "use client"
 
+import { useState } from "react";
+import { api } from "@/lib/api";
+
+
 const Home = () => {
-    async function handleTest() {
+    const [inputVal, setInputVal] = useState("")
+
+    const handleTest = async () => {
+        const rest = await api.get("/");
+        console.log(rest.data)
+
+    }
+
+    const handleSend = async () => {
         try {
-            const res = await fetch("http://localhost:4000/");
-            const data = await res.text();
-            console.log(data);
+            const rest = await api.post("/post1", { text: inputVal });
+            console.log(rest.data)
         } catch (err) {
             console.log(err)
         }
+
     }
 
     return (
-        <>
+        <div className="p-10 block">
             <p className="text-purple-500">hello</p>
             <button onClick={handleTest}>test</button>
-        </>
+            <input value={inputVal}
+                onChange={(e) => setInputVal(e.target.value)}
+                placeholder="podaj login"
+                className="border border-black w-75 block p-1"
+            />
+            <button className="p-2 bg-black text-white" onClick={handleSend}>send</button>
+        </div>
     )
 }
 
