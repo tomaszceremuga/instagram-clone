a76eea901bcee0bda24b747f7e1db4783fb05ee9
tomaszceremuga <tomaszceremuga>
2026-07-20 14:52:53 +0200

    add useAuth hook

---
+++ frontend/src/hooks/useAuth.ts
@@ -0,0 +1,31 @@
+import { api } from "@/lib/api";
+import { useEffect, useState } from "react";
+
+type User = {
+  id: number;
+  username: string;
+  email: string;
+};
+
+export const useAuth = () => {
+  const [user, setUser] = useState<User | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const res = await api.get("/me");
+        setUser(res.data);
+      } catch (err) {
+        console.error({ error: "couldn't authorise" });
+        setUser(null);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    checkAuth();
+  }, []);
+
+  return { user, isLoading };
+};
