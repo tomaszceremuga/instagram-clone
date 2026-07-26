c1f313f84669034c45dc430f5efc35c41ab4c460
tomaszceremuga <tomaszmceremuga@gmail.com>
2026-07-26 11:31:00 +0200

    finish desktop nav

--- frontend/src/components/ui/button.tsx
+++ frontend/src/components/ui/button.tsx
@@ -17,11 +17,12 @@ const buttonVariants = cva(
                 destructive:
                     "bg-destructive/10 text-destructive hover:bg-destructive/20 focus-visible:border-destructive/40 focus-visible:ring-destructive/20 dark:bg-destructive/20 dark:hover:bg-destructive/30 dark:focus-visible:ring-destructive/40",
                 link: "text-primary underline-offset-4 hover:underline",
-                "desktop-nav": "bg-red-200 rounded-lg p-6 w-fit flex justify-start  cursor-pointer",
+                "desktop-nav":
+                    "rounded-lg p-6 w-fit flex justify-start items-center gap-5 cursor-pointer hover:bg-gray-100  font-normal text-base",
             },
             size: {
                 default:
-                    "h-9 gap-1.5 px-3 has-data-[icon=inline-end]:pr-2.5 has-data-[icon=inline-start]:pl-2.5",
+                    "h-9 px-3 has-data-[icon=inline-end]:pr-2.5 has-data-[icon=inline-start]:pl-2.5",
                 xs: "h-6 gap-1 px-2.5 text-xs has-data-[icon=inline-end]:pr-2 has-data-[icon=inline-start]:pl-2 [&_svg:not([class*='size-'])]:size-3",
                 sm: "h-8 gap-1 px-3 has-data-[icon=inline-end]:pr-2 has-data-[icon=inline-start]:pl-2",
                 lg: "h-11 px-4",
